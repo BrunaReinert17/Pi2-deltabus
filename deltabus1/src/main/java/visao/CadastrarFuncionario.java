cbc66fc43891f2b34ce22f0159a3bbefd11b25e7
Maria Tereza <maria.w05@aluno.ifsc.edu.br>
2023-08-10 11:47:52 -0300

    telas login e cadastro

---
+++ deltabus1/src/main/java/visao/CadastrarFuncionario.java
@@ -0,0 +1,41 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class CadastrarFuncionario extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastrarFuncionario frame = new CadastrarFuncionario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadastrarFuncionario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(1000, 1000, 2000, 2000);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
--- deltabus1/src/main/java/visao/TelaLogin.java
+++ deltabus1/src/main/java/visao/TelaLogin.java
@@ -19,6 +19,7 @@ public class TelaLogin extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtEmail;
 	private JTextField txtSenha;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -51,35 +52,41 @@ public class TelaLogin extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(799, 305, 383, 502);
+		panel.setBounds(750, 176, 447, 549);
 		panel.setBackground(new Color(255, 255, 255));
 		panel.setForeground(new Color(204, 204, 204));
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(119, 383, 219, 42);
 		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBackground(new Color(47, 79, 79));
-		btnNewButton.setFont(new Font("Source Serif Pro Black", Font.PLAIN, 15));
-		btnNewButton.setBounds(76, 420, 219, 42);
+		btnNewButton.setBackground(new Color(0, 128, 128));
+		btnNewButton.setFont(new Font("Dialog", Font.BOLD, 18));
 		panel.add(btnNewButton);
 		
 		txtSenha = new JTextField();
-		txtSenha.setText("    Senha : ");
+		txtSenha.setBounds(87, 291, 274, 31);
+		txtSenha.setText("    Senha: ");
 		txtSenha.setForeground(Color.BLACK);
-		txtSenha.setFont(new Font("Source Serif Pro Black", Font.PLAIN, 13));
+		txtSenha.setFont(new Font("Dialog", Font.BOLD, 13));
 		txtSenha.setColumns(10);
-		txtSenha.setBackground(SystemColor.controlHighlight);
-		txtSenha.setBounds(49, 299, 274, 31);
+		txtSenha.setBackground(new Color(245, 245, 245));
 		panel.add(txtSenha);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(49, 225, 274, 31);
+		txtEmail.setBounds(87, 237, 274, 31);
 		panel.add(txtEmail);
-		txtEmail.setBackground(SystemColor.controlHighlight);
+		txtEmail.setBackground(new Color(245, 245, 245));
 		txtEmail.setForeground(new Color(0, 0, 0));
-		txtEmail.setFont(new Font("Source Serif Pro Black", Font.PLAIN, 13));
-		txtEmail.setText("    Email : ");
+		txtEmail.setFont(new Font("Dialog", Font.BOLD, 13));
+		txtEmail.setText("    Email: ");
 		txtEmail.setColumns(10);
+		
+		lblNewLabel = new JLabel("DELTABUS");
+		lblNewLabel.setForeground(new Color(0, 128, 128));
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 30));
+		lblNewLabel.setBounds(141, 130, 163, 31);
+		panel.add(lblNewLabel);
 	}
 }
