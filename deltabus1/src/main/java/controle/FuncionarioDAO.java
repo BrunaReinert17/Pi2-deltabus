d266b87e74cddef87bd148eb56372565fa032bf6
Águida Ariane <aguida.s@aluno.ifsc.edu.br>
2023-08-17 12:00:08 -0300

    ATUALIZAÇÃO

--- deltabus1/src/main/java/controle/FuncionarioDAO.java
+++ deltabus1/src/main/java/controle/FuncionarioDAO.java
@@ -1,5 +1,27 @@
 package controle;
 
-public class FuncionarioDAO {
+import java.util.ArrayList;
 
+import modelo.Funcionario;
+
+public class FuncionarioDAO{
+    
+	private static ArrayList<Funcionario> listaFuncionario;
+
+	private static FuncionarioDAO funcionarioDao;
+
+	public static void getIntancia() {
+		if (funcionarioDao == null) {
+			funcionarioDao = new FuncionarioDAO();
+			listaFuncionario = new ArrayList<Funcionario>();
+			
+			listaFuncionario.add(
+					new Funcionario("Bruna", 14058567937l, "bruna@gmail.com", "secretaria", "bruna18"));
+			listaFuncionario.add(new Funcionario("agatha",123456789101l,"agatha@gmail.com","vice secretaria","agatha12"));
+			listaFuncionario.add(new Funcionario("Maria Tereza",123456789101l,"maria@gmail.com","gerente","maria05"));
+			listaFuncionario.add(new Funcionario("Aguida",123456789101l,"aguida@gmail.com","vice gerente","aguida123"));
+			}
+	
+	
+}
 }
