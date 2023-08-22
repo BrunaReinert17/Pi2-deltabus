d9843703676452f884a49c6946d942ddc6f77ab5
BrunaReinert17 <carolinereinertbruna@gmail.com>
2023-08-22 13:37:48 -0300

    SCRIPT RODOUUU

--- scriptPi2.sql
+++ scriptPi2.sql
@@ -1,12 +1,12 @@
--- DROP DATABASES deltaBus IF EXISTS --
+-- DROP DATABASES deltaBus IF EXISTS 
 
-CREATE SCHEMA IF NOT EXISTS `deltaBus` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `deltaBus` ;
+CREATE SCHEMA IF NOT EXISTS `deltaBus2` ;
+USE deltabus2;
 
 -- -----------------------------------------------------
 -- Table `deltaBus`.`Veiculo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `deltaBus`.`Veiculo` (
+CREATE TABLE IF NOT EXISTS `Veiculo` (
   `idVeiculo` BIGINT(45) NOT NULL auto_increment,
   `marca` VARCHAR(45) NOT NULL,
   `modelo` VARCHAR(45) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `deltaBus`.`Veiculo` (
 -- -----------------------------------------------------
 -- Table `deltaBus`.`endereco`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `deltaBus`.`endereco` (
+CREATE TABLE IF NOT EXISTS `endereco` (
   `cep` INT NOT NULL,
   `cidade` VARCHAR(45) NOT NULL,
   `bairro` VARCHAR(45) NOT NULL,
@@ -35,44 +35,43 @@ CREATE TABLE IF NOT EXISTS `deltaBus`.`endereco` (
   `UF` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`cep`));
 
-
-CREATE TABLE IF NOT EXISTS `deltaBus`.`Clientes` (
+-- -----------------------------------------------------
+-- Table `deltaBus`.`Clientes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Clientes` (
   `Nome`  VARCHAR(255) NOT NULL,
   `numeroTelefone` INT NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `cpf` DOUBLE NOT NULL,
-  `cnpj` INT NOT NULL AUTO_INCREMENT,
+  `cnpj` BIGINT NOT NULL,
   `endereco_cep` INT NOT NULL,
   PRIMARY KEY (`cnpj`),
-  CONSTRAINT `fk_cadastrarClientes_endereco1`
     FOREIGN KEY (`endereco_cep`)
-    REFERENCES `deltaBus`.`endereco` (`cep`));
+    REFERENCES `endereco` (`cep`));
 
 
 -- -----------------------------------------------------
 -- Table `deltaBus`.`Pedido`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `deltaBus`.`Pedido` (
-  `dataCompra` DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS `Pedido` (
+`id_pedidos` int not null auto_increment,
+  `dataCompra` DATE NOT NULL ,
   `valorPago` DOUBLE NOT NULL,
   `tipoPagamento` VARCHAR(45) NOT NULL,
-  `pedidoIs` VARCHAR(45) NOT NULL,
   `Veiculo_idVeiculo` BIGINT(45) NOT NULL,
-  `Clientes_cnpj` INT NOT NULL,
-  PRIMARY KEY (`pedidoIs`, `Veiculo_idVeiculo`, `Clientes_cnpj`),
-  CONSTRAINT `fk_Pedido_Veiculo`
+  `Clientes_cnpj` BIGINT NOT NULL,
+  PRIMARY KEY (`id_pedidos`),
     FOREIGN KEY (`Veiculo_idVeiculo`)
-    REFERENCES `deltaBus`.`Veiculo` (`idVeiculo`),
-  CONSTRAINT `fk_Pedido_Clientes1`
+    REFERENCES `Veiculo` (`idVeiculo`),
     FOREIGN KEY (`Clientes_cnpj`)
-    REFERENCES `deltaBus`.`Clientes` (`cnpj`));
+    REFERENCES `Clientes` (`cnpj`));
 
 
 -- -----------------------------------------------------
 -- Table `deltaBus`.`Usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `deltaBus`.`Usuario` (
-  `idUsuario` VARCHAR(25) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Usuario` (
+  `idUsuario` int NOT NULL auto_increment,
   `senha` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `cargo` VARCHAR(45) NOT NULL,
@@ -81,22 +80,22 @@ CREATE TABLE IF NOT EXISTS `deltaBus`.`Usuario` (
 -- ----------------------------------------------------
 -- Table `deltaBus`.`funcionarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `deltaBus`.`funcionarios` (
-  `cpf` DOUBLE NOT NULL,
-  `nome` VARCHAR(25) NOT NULL,
+CREATE TABLE IF NOT EXISTS funcionarios (
+  `cpf` int NOT NULL,
+  `nome` VARCHAR(255) NOT NULL,
   `dataNascimento` VARCHAR(45) NOT NULL,
   `genero` VARCHAR(45) NOT NULL,
   `numerotelefone` INT NOT NULL,
   `email` VARCHAR(35) NOT NULL,
-  `Usuario_idUsuario` VARCHAR(25) NOT NULL,
+  `Usuario_idUsuario` int NOT NULL,
   `endereco_cep` INT NOT NULL,
   PRIMARY KEY (`cpf`),
-  CONSTRAINT `fk_funcionarios_Usuario1`
+
     FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `deltaBus`.`Usuario` (`idUsuario`),
-  CONSTRAINT `fk_funcionarios_endereco1`
+    REFERENCES `Usuario` (`idUsuario`),
+ 
     FOREIGN KEY (`endereco_cep`)
-    REFERENCES `deltaBus`.`endereco` (`cep`));
+    REFERENCES `endereco` (`cep`));
     
     -- SELECTS--
 
@@ -112,9 +111,6 @@ SELECT * FROM endereco ORDER BY cep ASC;
 SELECT COUNT(*) FROM Clientes; 
 SELECT * FROM Clientes ORDER BY cnpj ASC;
 
--- Select Pedido --
-SELECT COUNT(*) FROM Pedido;
-SELECT * FROM Pedido ORDER BY pedidoIs ASC;
 
 -- Selects Usuario -- 
 SELECT COUNT(*) FROM Usuario;
@@ -125,29 +121,33 @@ SELECT COUNT(*) FROM funcionarios;
 SELECT * FROM funcionarios ORDER BY cpf ASC;
 
 -- INSERT Veiculo --
-INSERT INTO Veiculo (idVeiculo,marca, modelo, preco, ano, acessorios, lotacao, cor, tipoFrota, tipoCombustivel, consultarEstoque_Codigoveiculo, placa, renavam, situacao) 
-VALUES ('6','Marcopolo', 'Paradiso G8 1050', 100000000.00, '2023-08-17', 'Ar condicionado, GPS', 5, 'Azul', 'Passeio', 'diesel', 123, 'ABC123', '123456789', 2);
+-- NAO VAI ID 
+INSERT INTO Veiculo (marca, modelo, preco, ano, acessorios, lotacao, cor, tipoFrota, tipoCombustivel, consultarEstoque_Codigoveiculo, placa, renavam, situacao) 
+VALUES ('Marcopolo', 'Paradiso G8 1050', 100000000.00, '2023-08-17', 'Ar condicionado, GPS', 5, 'Azul', 'Passeio', 'diesel', 123, 'ABC123', '123456789', 2);
 
 -- INSERT endereco --
+
+-- NAO PODE CEO REPETIDO 
 INSERT INTO endereco (cep, cidade, bairro, rua, estado, UF)
-VALUES ('896116815', 'Blumenau', 'Progresso', 'Rua Ernestine Ehrhardt', 'Santa Catarina', 'SC');
+VALUES ('21346560', 'Blumenau', 'Progresso', 'Rua Ernestine Ehrhardt', 'Santa Catarina', 'SC');
 
 -- INSERT  Clientes--
-INSERT INTO Clientes (Nome, numeroTelefone, email, cnpj, endereco_cep) 
-VALUES ('Gisele', '1234567890', 'gisele@gmail.com', '90.857.326/1254-63', '12345');
+-- NAO PODE CPF REPETIDO 
+INSERT INTO Clientes (Nome, numeroTelefone, email, cpf,cnpj, endereco_cep) 
+VALUES ("Gisele" , '1234567890', 'gisele@gmail.com', 4545545448,464646546468436, 21346560);
 
 -- INSERT Pedido --
-INSERT INTO Pedido (dataCompra, valorPago, tipoPagamento, pedidoIs, Veiculo_idVeiculo,Clientes_cnpj) 
-VALUES ('2023-08-16', 15000.00, 'Cartao','PED001', 1,'73.586.914/0572.68');
+-- NAO COLOCA ID, O proprio banco COLOCA
+INSERT INTO Pedido (dataCompra, valorPago, tipoPagamento, Veiculo_idVeiculo,Clientes_cnpj) 
+VALUES ('2023-08-16', 15000.00, 'Cartao',1,'464646546468436');
 
 --  INSERT Usuario --
-INSERT INTO Usuario (idUsuario, senha, email, cargo)
-VALUES ('Bruna', '1312', 'bruna@gmail.com', 'funcionario');
+INSERT INTO Usuario ( senha, email, cargo)
+VALUES ( '1312', 'bruna@gmail.com', 'funcionario');
 
 -- INSERTadministrador--
 --- INSERT INTO adiministrador ( idEmail, senha) VALUES ('agatha.c2009@gmail.com','Agatha')---
 
 -- INSERT funcionarios --
-INSERT INTO funcionarios (cpf, nome, dataNascimento, genero, numerotelefone,email, Usuario_idUsuario,endereco_cep) 
-VALUES (12345678901, 'Agatha Cristine Onofre Ribeiro','2004-01-19','Feminino', 987654321, 'agatha.cor@gmail.com', 'agatha_cor', 123456);
-
+INSERT INTO funcionarios (cpf, nome, dataNascimento, genero, numerotelefone, email, Usuario_idUsuario, endereco_cep) 
+VALUES ('15678901', 'Agatha Cristine Onofre Ribeiro', '2004-01-19', 'Feminino', 987654321, 'agatha.cor@gmail.com', 1, 21346560);
